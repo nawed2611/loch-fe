40c9b46b2e9fd1db51d1f0623d6e4c3a38310889
nawed2611 <nawed2611@gmail.com>
2023-11-16 00:23:12 +0530

    Fix: Update changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ import { useEffect, useRef, useState } from "react";
 import { Toaster, toast } from 'sonner';
 
 const Card1 = () => (
-  <div className="flex flex-col card-bg text-black w-1/2 p-4 rounded-lg gap-y-2">
+  <div className="flex flex-col card-bg text-black w-42 p-4 rounded-lg gap-y-2">
     <div className="flex items-center justify-between">
-      <Image src="./bell2.svg" width={25} height={25} alt="Bell Image" />
-      <button className="rounded-lg p-2 font-semibold text-xs">Save</button>
+      <Image src="./bell2.svg" width={20} height={20} alt="Bell Image" />
+      <button className="rounded-lg font-semibold text-xs">Save</button>
     </div>
-    <p className="w-3/4 text-sm">
+    <p className="w-3/4 text-sm pt-2">
       We’ll be sending
       notifications to you
       here
@@ -25,31 +25,38 @@ const Card1 = () => (
   </div>
 )
 
-const Card2 = () => (
-  <div className="flex flex-col card-bg text-black w-1/2 p-4 rounded-lg gap-y-2">
-    <div className="flex items-center justify-between">
-      <Image src="./barchart.svg" width={25} height={25} alt="Bell Image" />
-      <button className="rounded-lg p-2 font-semibold text-xs">Save</button>
-    </div>
-    <p className="w-3/4 text-sm">
-      Notify me when any wallets
-      move more than
-    </p>
+const Card2 = () => {
+  const [selectedOption, setSelectedOption] = useState('');
 
-    <form className="flex flex-col pt-2">
-      <input className="focus:outline-none placeholder-black border p-2 text-black rounded-md text-xs" type="text" placeholder="hello@gmail.com" />
-    </form>
-  </div>
-)
+  const handleInputChange = (e: any) => {
+    setSelectedOption(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col card-bg text-black w-42 p-4 rounded-lg gap-y-2">
+      <div className="flex items-center justify-between">
+        <Image src="./barchart.svg" width={20} height={20} alt="Bell Image" />
+        <Image src="./checkmark.svg" width={20} height={20} alt="Bell Image" />
+      </div>
+      <p className="w-3/4 text-sm pt-2">
+        Notify me when any wallets
+        move more than
+      </p>
+
+      <form className="flex flex-col pt-2">
+        <input className="focus:outline-none placeholder-black border p-2 text-black rounded-md text-xs" type="text" placeholder="hello@gmail.com" />
+      </form>
+    </div>
+  )
+}
 
 const Card3 = () => (
-  <div className="flex flex-col card-bg text-black w-1/2 p-4 rounded-lg gap-y-2">
+  <div className="flex flex-col card-bg text-black w-42 p-4 rounded-lg gap-y-2">
     <div className="flex items-center justify-between">
-      <Image src="./clock.svg" width={25} height={25} alt="Bell Image" />
-
-      <button className="rounded-lg p-2 font-semibold text-xs">Save</button>
+      <Image src="./clock.svg" width={20} height={20} alt="Bell Image" />
+      <Image src="./checkmark.svg" width={20} height={20} alt="Bell Image" />
     </div>
-    <p className="w-3/4 text-sm">
+    <p className="w-3/4 text-sm pt-2">
       Notify me when any wallet dormant for
     </p>
 
@@ -112,23 +119,23 @@ export default function Home() {
 
           <div className="flex sm:flex-row flex-col items-center justify-between">
             <Image src="./cohorts.svg" width={420} height={420} alt="Cohorts Image" />
-            <div className="flex flex-col gap-4 w-full p-6 sm:p-0 sm:w-1/3 items-start sm:items-end pr-2 mx-12">
+            <div className="flex flex-col gap-4 w-full p-6 sm:p-0 sm:w-1/3 items-end pr-12 sm:pr-2 mx-4 sm:mx-12">
               <Image className="" src="./eye.svg" width={40} height={40} alt="Bell Image" />
-              <h1 className='text-left sm:text-right text-3xl'>Watch what the whales are doing</h1>
-              <p className="text-left sm:text-right opacity-70">All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.</p>
+              <h1 className='text-right text-3xl'>Watch what the whales are doing</h1>
+              <p className="text-right opacity-70">All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.</p>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col items-end gap-y-4 h-[80vh] sm:h-fit sm:p-0 sm:pt-24 pt-8">
+        <div className="flex flex-col items-end gap-y-4 h-[40vh] sm:h-fit sm:p-0 sm:pt-28 pt-8">
           <h1 className="text-2xl pr-12">Testimonials</h1>
           <div className="sm:w-[90%] w-full border-b border-gray-200 opacity-40 mx-12 mb-2"></div>
 
           <div className="flex sm:flex-row flex-col sm:items-end justify-between w-full gap-6 sm:gap-0">
             <Image className="ml-12" src="./vector.svg" width={60} height={60} alt="Vector Image" />
 
-            <motion.div ref={repoContainer} whileTap={{ cursor: "grabbing" }} className="cursor-grab flex overflow-hidden w-full p-4 sm:p-0 sm:w-4/5 rounded-l-xl drop-shadow-2xl">
-              <motion.div drag="x" dragConstraints={{ right: 0, left: -700 }} className="flex  gap-4 ">
+            <motion.div ref={repoContainer} whileTap={{ cursor: "grabbing" }} className="cursor-grab flex overflow-hidden w-full p-4 sm:p-0 sm:w-3/4 rounded-l-xl drop-shadow-2xl">
+              <motion.div drag="x" dragConstraints={{ right: 0, left: -500 }} className="flex  gap-4 ">
                 <TestimonialCard name="Jack F" title="Ex Blackrock PM" content="“Love how Loch integrates portfolio analytics and whale watching into one unified app.”" />
                 <TestimonialCard name="Yash P" title="Research, 3poch Crypto Hedge Fund" content="“I use Loch everyday now. I don't think I could analyze crypto whale trends markets without it. I'm addicted!”" />
                 <TestimonialCard name="Shiv S" title="Co-Founder Magik Labs" content="“Managing my own portfolio is helpful and well designed. What’s really interesting is watching the whales though. No one else has made whale tracking so simple.”" />
@@ -138,7 +145,7 @@ export default function Home() {
         </div>
 
       </div>
-      <div className='flex flex-col items-center justify-center h-[80vh] sm:h-screen w-full sm:w-[35vw] bg-white drop-shadow-2xl gap-4'>
+      <div className='flex flex-col items-center justify-center h-[80vh] sm:h-screen w-full sm:w-[35vw] bg-white drop-shadow-4xl gap-4'>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-2/3">
           <h1 className="text-4xl text-gray-300 pb-4">Sign up for exclusive access.</h1>
           <input className="focus:outline-none drop-shadow-lg border border-gray-200 text-black rounded-lg p-4" type="text" value={email} onChange={(e: any) => setEmail(e.target.value)} placeholder="Your email address" />
--- components/testimonial/card.tsx
+++ components/testimonial/card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const TestimonialCard: React.FC<CardProps> = ({ name, title, content }) => {
     return (
-        <div className="flex flex-col gap-4 text-black bg-gray-200 p-4 sm:h-40 w-[70vw] sm:w-[30vw] rounded-xl">
+        <div className="text-sm flex flex-col gap-4 text-black bg-gray-200 p-4 sm:h-34 w-[70vw] sm:w-[25vw] rounded-xl">
             <div className='flex flex-col sm:flex-row sm:items-center gap-4'>
                 <h1 className='font-semibold'>{name}</h1>
                 <h2 className='text-gray-400'>{title}</h2>
