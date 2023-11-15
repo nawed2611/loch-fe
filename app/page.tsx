040ab169370abbd7ff72db3c296cf4428f178c67
nawed2611 <nawed2611@gmail.com>
2023-11-16 00:31:53 +0530

    Fix: Add changes

--- app/page.tsx
+++ app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
         </div>
 
       </div>
-      <div className='flex flex-col items-center justify-center h-[60vh] sm:h-screen w-full sm:w-[35vw] bg-white drop-shadow-4xl gap-4'>
+      <div className='flex flex-col items-center justify-center h-[60vh] sm:h-screen w-full sm:w-[35vw] bg-white rounded-sm drop-shadow-4xl gap-4'>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-2/3">
           <h1 className="text-4xl text-gray-300 pb-4">Sign up for exclusive access.</h1>
           <input className="focus:outline-none drop-shadow-lg border border-gray-200 text-black rounded-lg p-4" type="text" value={email} onChange={(e: any) => setEmail(e.target.value)} placeholder="Your email address" />
